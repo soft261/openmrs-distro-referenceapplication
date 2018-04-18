b8d14382ed792acf30f6ff97fc05d26bb9bee4cd
Adam <amsmann@cableone.net>
2018-04-18 12:27:58 -0500

    Nearly completed test

--- ui-tests/src/test/java/org/openmrs/reference/LoginAfterSessionExpirationTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/LoginAfterSessionExpirationTest.java
@@ -3,10 +3,13 @@ package org.openmrs.reference;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.openmrs.reference.page.HomePage;
+import org.openmrs.reference.page.AppointmentSchedulingPage;
+import org.openmrs.reference.page.ManageProviderSchedulesPage;
 
 public class LoginAfterSessionExpirationTest extends ReferenceApplicationTestBase {
     private HomePage homePage;
@@ -21,8 +24,12 @@ public class LoginAfterSessionExpirationTest extends ReferenceApplicationTestBas
         assertTrue(homePage.isAppointmentSchedulingAppPresent());
     	assertTrue(homePage.isCaptureVitalsAppPresent());
         assertThat(homePage.numberOfAppsPresent(), is(4));
-        Thread.sleep(1800000);
-        homePage.clickOn();
-        
+    	AppointmentSchedulingPage appointmentSchedulingPage = homePage.goToAppointmentScheduling();
+    	assertPage(appointmentSchedulingPage);
+        Thread.sleep(1800100);
+        appointmentSchedulingPage.goToManageAppointments();
+        assertPage(getLoginPage());
+        goToLoginPage().loginAsNurse();
+        assertPage(appointmentSchedulingPage);        
     }
 } 
