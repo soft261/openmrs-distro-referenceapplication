7a51a05863430461604411d0be17c8e0a34318d1
Adam <amsmann@cableone.net>
2018-04-19 19:46:43 -0500

    Commented lines to clarify what is happening

--- ui-tests/src/test/java/org/openmrs/reference/LoginAfterSessionExpirationTest.java
+++ ui-tests/src/test/java/org/openmrs/reference/LoginAfterSessionExpirationTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.openmrs.reference.page.HomePage;
+import org.openmrs.reference.groups.BuildTests;
 import org.openmrs.reference.page.AppointmentSchedulingPage;
 import org.openmrs.reference.page.ManageProviderSchedulesPage;
 
@@ -15,21 +16,21 @@ public class LoginAfterSessionExpirationTest extends ReferenceApplicationTestBas
     private HomePage homePage;
 
     @Test
+    @Category(BuildTests.class)
     public void loginAfterSessionExpires() throws Exception { 
-    	goToLoginPage().loginAsNurse();
+    	//goToLoginPage().loginAsNurse();
     	homePage = new HomePage(page);
-    	assertPage(homePage);
-    	assertTrue(homePage.isFindAPatientAppPresent());
-    	assertTrue(homePage.isActiveVisitsAppPresent());
-        assertTrue(homePage.isAppointmentSchedulingAppPresent());
-    	assertTrue(homePage.isCaptureVitalsAppPresent());
-        assertThat(homePage.numberOfAppsPresent(), is(4));
+    	login();
+    	// Go to a new page
     	AppointmentSchedulingPage appointmentSchedulingPage = homePage.goToAppointmentScheduling();
     	assertPage(appointmentSchedulingPage);
-        Thread.sleep(1800100);
+        // Wait for 30 minutes for the session timeout. We added 15 seconds just as a cushion.
+    	Thread.sleep(1815000);
+    	// Try to click on something
         appointmentSchedulingPage.goToManageAppointments();
-        assertPage(getLoginPage());
-        goToLoginPage().loginAsNurse();
+        // Should be sent back to the login page
+        login();
+        // Logging in again should restore to the previous page.
         assertPage(appointmentSchedulingPage);        
     }
 } 
